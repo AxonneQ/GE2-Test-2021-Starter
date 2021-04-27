99a5d56c6f2ec58b89a1de4b1bc122fa7607559a
AxonneQ <bolek.igor.94@gmail.com>
2021-04-27 10:02:01 +0100

    Add Throwing forward

--- Assets/Throw.cs
+++ Assets/Throw.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Throw : MonoBehaviour
 {
     public Transform spawnPoint;
-    public GameObject ballPrefab; 
-
+    public GameObject ballPrefab;
+   
     public float throwCooldown = 3;
 
     // Start is called before the first frame update
@@ -18,8 +18,9 @@ public class Throw : MonoBehaviour
     void Shoot()
     {
         GameObject ball = GameObject.Instantiate<GameObject>(ballPrefab);
-        ball.transform.position = spawnPoint.position;
+        ball.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane + 2) );
         ball.transform.rotation = this.transform.rotation;
+        ball.transform.GetComponent<Rigidbody>().velocity = ball.transform.forward * 20;
     }
 
     void OnEnable()
