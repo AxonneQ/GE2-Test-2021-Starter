b46c32b73f47dabb00ecb9cccdfa9ba190d239fc
AxonneQ <bolek.igor.94@gmail.com>
2021-04-27 09:33:38 +0100

    Add Throw method

---
+++ Assets/Throw.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject ballPrefab; 
+
+    public float throwCooldown = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        GameObject ball = GameObject.Instantiate<GameObject>(ballPrefab);
+        ball.transform.position = spawnPoint.position;
+        ball.transform.rotation = this.transform.rotation;
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(ThrowingCoroutine());
+    }
+
+    bool shooting = false;
+
+    System.Collections.IEnumerator ThrowingCoroutine()
+    {
+        while(true)
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                Shoot();
+                yield return new WaitForSeconds(throwCooldown);
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
